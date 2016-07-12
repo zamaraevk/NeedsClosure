324ed3d925be9e146234e7b93e96ef9306d548e3
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-07-11 21:58:42 -0400

    add Task to database -- Complete

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,9 +1,8 @@
 angular.module('services', [])
 
   //None of these functions will be used until a functioning server is in place.
-
-  .controller('Tasks', function($http){
-    var fetchAllTasks = function(){
+.factory('Tasks', function ($http) {
+  var fetchAllTasks = function(){
       return $http({
         method: 'GET',
         url: '/api/tasks'
@@ -12,37 +11,65 @@ angular.module('services', [])
       })
     };
 
-    var addTask = function(task){
-      return $http({
-        method: 'POST',
-        url: '/api/tasks',
-        data: link
-      }).then(function(resp){
-        console.log('Task Successfully Added.');
-      })
-    };
+  var addTask = function(task){
+    console.log('working', task);
+    return $http({
+      method: 'POST',
+      url: '/api/tasks',
+      data: task
+    }).then(function(resp){
+      console.log('Task Successfully Added.');
+    })
+  };
 
-    var deleteTask = function(task){
-      return $http({
-        method: 'DELETE',
-        url: '/api/tasks',
-        data: task
-      }).then(function(resp){
-        console.log('Task Successfully Deleted.');
-      })
-    };
+  return {
+    fetchAllTasks:fetchAllTasks,
+    addTask: addTask
+  }
 
-    var completeTask = function(task){
-      return $http({
-        method: 'PUT',
-        url: '/api/tasks',
-        data: task
-      })
-    }
+})
 
-  })
+  // .controller('Tasks', function($http){
+  //   var fetchAllTasks = function(){
+  //     return $http({
+  //       method: 'GET',
+  //       url: '/api/tasks'
+  //     }).then(function(resp){
+  //       return resp.data;
+  //     })
+  //   };
+  //
+  //   var addTask = function(task){
+  //     return $http({
+  //       method: 'POST',
+  //       url: '/api/tasks',
+  //       data: link
+  //     }).then(function(resp){
+  //       console.log('Task Successfully Added.');
+  //     })
+  //   };
+  //
+  //   var deleteTask = function(task){
+  //     return $http({
+  //       method: 'DELETE',
+  //       url: '/api/tasks',
+  //       data: task
+  //     }).then(function(resp){
+  //       console.log('Task Successfully Deleted.');
+  //     })
+  //   };
+  //
+  //   var completeTask = function(task){
+  //     return $http({
+  //       method: 'PUT',
+  //       url: '/api/tasks',
+  //       data: task
+  //     })
+  //   }
+  //
+  // })
 
-  //These functions can be used to wrap MVP in an authentication service. 
+  //These functions can be used to wrap MVP in an authentication service.
 
   .factory('Auth', function ($http, $location, $window) {
     var signin = function (user) {
