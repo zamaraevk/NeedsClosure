6a8f355ff7cf6f142199eab8d2a49f1d68c5a12c
tgrander <treygranderson@gmail.com>
2016-07-13 16:19:24 -0400

    Fixed Auth error

--- client/app/tasks/tasks.js
+++ client/app/tasks/tasks.js
@@ -2,7 +2,7 @@ angular.module('tasks', [])
 
 .controller('TasksController', function($scope, Tasks){
 
-  angular.extend($scope, Tasks, Auth);
+  angular.extend($scope, Tasks);
   //will be submitted to server in POST request body containing the new task input data
     //when ready to send requests to server, add "Tasks" controller as function input variable
   $scope.allTasks = [];
