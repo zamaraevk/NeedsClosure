6dd3bcd1b3c8daae48e506f63b407e5ce3e2b555
tgrander <treygranderson@gmail.com>
2016-07-14 19:18:38 -0400

    Prepared to work on projects feature

---
+++ client/app/projects/projects.js
@@ -0,0 +1,17 @@
+angular.module('projects', [])
+
+.controller('ProjController', function($scope){
+
+})
+.factory('Proj', function(){
+
+  //function to add project to projects list in index.html sidebar
+    //send POST request to server with the name of the new project that is entered
+    //in input field
+
+  
+
+  return {
+
+  }
+})
