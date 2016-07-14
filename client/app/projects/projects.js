80835d1bccc64720932f2369cb3363132c56fb4b
tgrander <treygranderson@gmail.com>
2016-07-14 19:51:09 -0400

    projects.html rough draft 1

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -1,7 +1,7 @@
 angular.module('projects', [])
 
 .controller('ProjController', function($scope){
-
+ $scope.task = {};
 })
 .factory('Proj', function(){
 
@@ -9,7 +9,11 @@ angular.module('projects', [])
     //send POST request to server with the name of the new project that is entered
     //in input field
 
-  
+  //POST request to add task to db
+
+  //GET request to search for usernames
+
+  //POST request to add new project category to db
 
   return {
 
