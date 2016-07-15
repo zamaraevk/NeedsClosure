22841eb07f044774e405510295c12ecec888e5fc
tgrander <treygranderson@gmail.com>
2016-07-15 10:12:25 -0400

    updates;

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -1,1 +1,21 @@
 
+.controller('ProjController', function($scope, Proj){
+
+  $scope.newProject = {};
+
+  $scope.addNewProj = function(){
+    Proj.addProject($scope.newProject);
+  };
+
+})
+
+.factory('Proj', function(){
+
+  //function to add project to projects list in index.html sidebar
+    //send POST request to server with the name of the new project that is entered
+    //in input field
+
+  return {
+
+  }
+})
