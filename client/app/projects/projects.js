48a18f5a641f5311510b9964974cf3dd92491bb6
tgrander <treygranderson@gmail.com>
2016-07-15 10:12:25 -0400

    updates;

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -1,15 +1,20 @@
 angular.module('projects', [])
 
-.controller('ProjController', function($scope){
+.controller('ProjController', function($scope, Proj){
+
+  $scope.newProject = {};
+  
+  $scope.addNewProj = function(){
+    Proj.addProject($scope.newProject);
+  };
 
 })
+
 .factory('Proj', function(){
 
   //function to add project to projects list in index.html sidebar
     //send POST request to server with the name of the new project that is entered
     //in input field
-
-  
 
   return {
 
